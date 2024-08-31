f17913725ca47ef73596b4421eeec289e2d909bf
dianjiu <dianjiuxyz@gmail.com>
2024-08-31 21:02:57 +0800

    fix:实现审计日志切面，异步入库

--- remi-comm-log/src/main/java/ai/remi/comm/log/service/AsyncLogService.java
+++ remi-comm-log/src/main/java/ai/remi/comm/log/service/AsyncLogService.java
@@ -1,7 +1,14 @@
 package ai.remi.comm.log.service;
 
+import ai.remi.comm.log.domain.OperateLog;
+import ai.remi.comm.util.auth.CompanyAuthInfo;
+import ai.remi.comm.util.bean.BeanCopyUtils;
+import ai.remi.sys.api.client.SysClientApi;
+import ai.remi.sys.domain.dto.post.LogRecordPostDTO;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * 异步调用日志服务
@@ -12,19 +19,19 @@ public class AsyncLogService {
     /**
      * 通过OpenFeign客户端注入
      */
-    //@Resource
-    //private IamClientApi iamClientApi;
+    @Resource
+    private SysClientApi sysClientApi;
 
     /**
      * 保存系统日志记录
      */
     @Async
-    public void saveOperateLog(ai.remi.comm.log.domain.OperateLog operateLog) {
-        //ai.remi.comm.util.auth.CompanyAuthInfo companyAuthInfo = new ai.remi.comm.util.auth.CompanyAuthInfo();
-        //companyAuthInfo.setDeptTenantId(operateLog.getDeptTenantId());
-        //companyAuthInfo.setCompanyTenantId(operateLog.getCompanyTenantId());
-        //ai.remi.comm.util.auth.RequestHolder.add(companyAuthInfo);
-        //LogRecordPostDTO logRecordPostDTO = ai.remi.comm.util.bean.BeanCopyUtils.copyPropertiesThird(operateLog, LogRecordPostDTO.class);
-        //iamClientApi.addLogRecord(logRecordPostDTO);
+    public void saveOperateLog(OperateLog operateLog) {
+        CompanyAuthInfo companyAuthInfo = new CompanyAuthInfo();
+        companyAuthInfo.setDeptTenantId(operateLog.getDeptTenantId());
+        companyAuthInfo.setCompanyTenantId(operateLog.getCompanyTenantId());
+        ai.remi.comm.util.auth.RequestHolder.add(companyAuthInfo);
+        LogRecordPostDTO logRecordPostDTO = BeanCopyUtils.copyPropertiesThird(operateLog, LogRecordPostDTO.class);
+        sysClientApi.addLogRecord(logRecordPostDTO);
     }
 }
